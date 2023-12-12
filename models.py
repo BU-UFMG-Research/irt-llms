5de01620a9eb2f3bc7b43049c029896b767c3346
gaabrielfranco <gabrielvsfranco@outlook.com>
2023-12-11 20:51:29 -0500

    Many changes

--- models.py
+++ models.py
@@ -1,6 +1,6 @@
 from abc import ABC, abstractmethod
 import re
-from transformers import LlamaForCausalLM , LlamaTokenizer, set_seed
+from transformers import LlamaForCausalLM , LlamaTokenizer, AutoModelForCausalLM, AutoTokenizer, BitsAndBytesConfig, set_seed
 import torch
 
 # Define abstract model class
@@ -16,31 +16,102 @@ class Model(ABC):
     def create_prompt(self, question, system_prompt_type=None):
         pass
 
-    def parse_answer(self, answer):
-        ans = re.search('\([ABCDE]\)',answer).group().lstrip('(').rstrip(')') if re.search('\([ABCDE]\)',answer) else None
+    def parse_answer(self, answer, question):
+        pos_inst = answer.split('[/INST]')[-1]
+        ans = pos_inst.strip()
+
+        pattern = r'answer is ([A-E])'
+        match = re.search(pattern, ans)
+        if match:
+            return match.group(1)
+        
+        pattern = r'answer is \(([A-E])\)'
+        match = re.search(pattern, ans)
+        if match:
+            return match.group(1)
+        
+        pattern = r'Answer: ([ABCDE])'
+        match = re.search(pattern, ans)
+        if match:
+            return match.group(1)
+        
+        pattern = r'A resposta correta é (\w):'
+        match = re.search(pattern, ans)
+        if match:
+            return match.group(1)
+        
+        pattern = r'[Tt]he answer is ([A-E])'
+        match = re.search(pattern, ans)
+        if match:
+            return match.group(1)
+        
+        pattern = r'answer is \(([A-E])\)'
+        match = re.search(pattern, ans)
+        if match:
+            return match.group(1)
+            
+        pattern = r'A resposta correta é (\w) '
+        match = re.search(pattern, ans)
+        if match:
+            return match.group(1)
+
+        pattern = r'A resposta certa é (\w):'
+        match = re.search(pattern, ans)
+        if match:
+            return match.group(1)
+        
+        pattern = r'option ([A-E])'
+        match = re.search(pattern, ans)
+        if match:
+            return match.group(1)
+
+        if re.match(r"^(A|B|C|D|E)$", ans):
+            return ans
+        
+        pattern = r'\(([A-E])\)'
+        match = re.search(pattern, ans)
+        if match:
+            return match.group(1)
+    
+        ans = re.search('[ABCDE]\.',ans).group().rstrip('.') if re.search('[ABCDE]\.',ans) else None
+        
         if ans is None:
-            ans = answer.split('[/INST]')[-1]
-            ans = re.search('[ABCDE]\.',ans).group().rstrip('.') if re.search('[ABCDE]\.',ans) else None
+            pos_inst = answer.split('[/INST]')[-1]
+            ans = pos_inst.strip()
+            ans = re.search('[ABCDE]\)',ans).group().rstrip(')') if re.search('[ABCDE]\)',ans) else None
+
+        if ans is None and question is not None:
+            for option in ['A', 'B', 'C', 'D', 'E']:
+                if question[option] in pos_inst:
+                    return option
+        
         return ans
 
 
 # Define LLAMA2 model class
+
+"""
+4 bit quantization code
+
+from transformers import BitsAndBytesConfig
+
+
+nf4_config = BitsAndBytesConfig(
+   load_in_4bit=True,
+   bnb_4bit_quant_type="nf4",
+   bnb_4bit_use_double_quant=True,
+   bnb_4bit_compute_dtype=torch.bfloat16
+)
+
+model_nf4 = AutoModelForCausalLM.from_pretrained(model_id, quantization_config=nf4_config)
+
+
+"""
 class LLAMA2(Model):
     """
     LLAMA2 model class
-
-    Attributes:
-        model_size (str): model size
-        token (str): token
-        device (str): device
-        tokenizer (LlamaTokenizer): tokenizer
-        model (LlamaForCausalLM): model
-    
-    Methods:
-        get_answer_from_prompt: get answer from prompt
-        parse_answer: parse answer
     """
-    def __init__(self, model_size, token, device, max_time=120, temperature=0.6, random_seed=0):
+    def __init__(self, model_size, token, device, max_time=120, temperature=0.6, load_4bit=False, random_seed=0):
         """
         Args:
             model_size (str): model size
@@ -48,40 +119,108 @@ class LLAMA2(Model):
             device (str): device
         """
         super().__init__()
+
+        if load_4bit:
+            self.bnb_config = BitsAndBytesConfig(
+                load_in_4bit=True,
+                bnb_4bit_use_double_quant=True,
+                bnb_4bit_quant_type="nf4",
+                bnb_4bit_compute_dtype=torch.bfloat16
+            )
+
         self.tokenizer = LlamaTokenizer.from_pretrained(f"meta-llama/Llama-2-{model_size}-chat-hf", token=token)
-        self.model = LlamaForCausalLM.from_pretrained(f"meta-llama/Llama-2-{model_size}-chat-hf", token=token, device_map="auto", torch_dtype=torch.float16)
+        if load_4bit:
+            self.model = LlamaForCausalLM.from_pretrained(f"meta-llama/Llama-2-{model_size}-chat-hf", token=token, device_map="auto", quantization_config=self.bnb_config)
+        else:
+            self.model = LlamaForCausalLM.from_pretrained(f"meta-llama/Llama-2-{model_size}-chat-hf", token=token, device_map="auto", torch_dtype=torch.float16)
         self.model_size = model_size
         self.device = device
         self.max_time = max_time # Max time in seconds to generate an answer
         self.temperature = temperature
+        self.seed = random_seed
         set_seed(random_seed)
 
     def get_answer_from_question(self, question, system_prompt_type=None):
         """
         Get answer from question
-
-        Args:
-            question (dict): ENEM question
-            temperature (float): temperature
-            system_prompt_type (str): system prompt type (simple, chain-of-thought, llama2-paper). None for no system prompt
-
-        Returns:
-            str: parsed answer
         """
         prompt = self.create_prompt(question, system_prompt_type)
         inputs = self.tokenizer(prompt, return_tensors='pt').input_ids.to(self.device)
         outputs = self.model.generate(inputs, temperature=self.temperature, max_time=self.max_time) # We can check out the gen config by model.generation_config. More details in how to change the generation available in: https://huggingface.co/docs/transformers/generation_strategies
         full_answer = self.tokenizer.batch_decode(outputs, skip_special_tokens=True)[0]
-        return self.parse_answer(full_answer), full_answer
+        return self.parse_answer(full_answer, question), full_answer
     
-    def create_prompt(self, question, system_prompt_type=None):
-        # For the multi-turn prompt, we need to add <s> and </s> tokens
-        prompt = '[INST] '
-        if system_prompt_type == "simple":
-            prompt += '<<SYS>>\nYou are a machine designed to answer multiple choice questions with the correct alternative among A,B,C,D or E. Answer only with the correct alternative.\n<</SYS>>\n\n'
-        elif system_prompt_type == "cot":
-            raise NotImplementedError
-        elif system_prompt_type == "llama2":
-            prompt += '<<SYS>>\nYou are a helpful, respectful and honest assistant. Always answer as helpfully as possible, while being safe. Your answers should not include any harmful, unethical, racist, sexist, toxic, dangerous, or illegal content. Please ensure that your responses are socially unbiased and positive in nature. If a question does not make any sense, or is not factually coherent, explain why instead of answering something not correct. If you don\'t know the answer to a question, please don\'t share false information.\n<</SYS>>\n\n'
-        prompt += f'{question["body"]}\n\nA. {question["A"]}\nB. {question["B"]}\nC. {question["C"]}\nD. {question["D"]}\nE. {question["E"]}\n[/INST]'
+    def create_prompt(self, question, system_prompt_type=None, language="pt-br"):
+        """
+        Create prompt
+        """
+        # For the multi-turn prompt, we need to add <s> and </s> tokens and concatenate the previous turns
+
+        if language == "en":
+            question_word = "Question"
+            if system_prompt_type == "simple":
+                system_prompt = '\nYou are a machine designed to answer multiple choice questions with the correct alternative among (A),(B),(C),(D) ou (E). Answer only with the correct alternative.'
+            elif system_prompt_type == "cot":
+                raise NotImplementedError
+        elif language == "pt-br":
+            question_word = "Questão"
+            if system_prompt_type == "simple":
+                system_prompt = 'Você é uma máquina projetada para responder questões de múltipla escolha com a alternativa correta entre (A),(B),(C),(D) ou (E). Responda apenas com a alternativa correta.'
+            elif system_prompt_type == "cot":
+                raise NotImplementedError
+            
+        prompt = f"""<s>[INST] <<SYS>>\n{system_prompt}\n<</SYS>>\n\n{question_word}: {question["body"]}\n\n(A) {question["A"]}\n(B) {question["B"]}\n(C) {question["C"]}\n(D) {question["D"]}\n(E) {question["E"]} [/INST]\n"""
+
         return prompt
+    
+class Mistral(Model):
+    """
+    Mistral model class
+    """
+
+    def __init__(self, token, device, max_time=120, temperature=0.6, random_seed=0):
+        super().__init__()
+        self.tokenizer = AutoTokenizer.from_pretrained("mistralai/Mistral-7B-Instruct-v0.1", token=token)
+        self.model = AutoModelForCausalLM.from_pretrained("mistralai/Mistral-7B-Instruct-v0.1", token=token, device_map="auto", torch_dtype=torch.float16)
+        self.device = device
+        self.max_time = max_time
+        self.temperature = temperature
+        self.seed = random_seed
+        set_seed(random_seed)
+
+    def get_answer_from_question(self, question, system_prompt_type=None):
+        """
+        Get answer from question
+        """
+        prompt = self.create_prompt(question, system_prompt_type)
+        inputs = self.tokenizer(prompt, return_tensors='pt').input_ids.to(self.device)
+        outputs = self.model.generate(inputs, temperature=self.temperature, max_time=self.max_time, do_sample=True, top_p=0.9, top_k=0, max_length=4096)
+        full_answer = self.tokenizer.batch_decode(outputs, skip_special_tokens=True)[0]
+        return self.parse_answer(full_answer, question), full_answer
+    
+    def create_prompt(self, question, system_prompt_type=None, language="pt-br"):
+        """
+        Create prompt
+        """
+        # For the multi-turn prompt, we need to add <s> and </s> tokens and concatenate the previous turns
+
+        if language == "en":
+            question_word = "Question"
+            if system_prompt_type == "simple":
+                system_prompt = "You are a machine designed to answer multiple choice questions with the correct alternative among (A),(B),(C),(D) ou (E). Answer only with the correct alternative."
+            elif system_prompt_type == "cot":
+                raise NotImplementedError
+        elif language == "pt-br":
+            question_word = "Questão"
+            if system_prompt_type == "simple":
+                system_prompt = "Você é uma máquina projetada para responder questões de múltipla escolha com a alternativa correta entre (A),(B),(C),(D) ou (E). Responda apenas com a alternativa correta."
+            elif system_prompt_type == "cot":
+                raise NotImplementedError
+            
+        prompt = f"""<s>[INST] {system_prompt}\n\n{question_word}: {question["body"]}\n\n(A) {question["A"]}\n(B) {question["B"]}\n(C) {question["C"]}\n(D) {question["D"]}\n(E) {question["E"]} [/INST]"""
+
+        return prompt
+
+
+
+
--- run_enem_exam.py
+++ run_enem_exam.py
@@ -5,7 +5,7 @@ import argparse
 import time
 import pandas as pd
 import torch
-from models import LLAMA2
+from models import LLAMA2, Mistral
 from exam import ENEM
 
 """
@@ -23,13 +23,15 @@ GenerationConfig {
 
 # Create an argparser
 parser = argparse.ArgumentParser(description='Run model on ENEM exam')
-parser.add_argument('--model', type=str, choices=["llama2"], required=True, help='Model to run')
+parser.add_argument('--model', type=str, choices=["llama2", "mistral"], required=True, help='Model to run')
 parser.add_argument('--model_size', type=str, choices=["7b", "13b"], required=True, help='Model size')
 parser.add_argument('--enem_exam', type=str, required=True, help='ENEM exam to run')
 parser.add_argument('--temperature', type=float, default=0.6, help='Temperature')
 parser.add_argument('--answer_order', type=str, default="ABCDE", help='Answer order')
 parser.add_argument('--question_order', type=str, default="original", choices=["original", "random"], help='Question order on ENEM exam. In random order, questions are shuffled using the seed to control the randomness')
-parser.add_argument('--system_prompt_type', type=str, default="simple", choices=["simple", "cot", "llama2"], help='System prompt type')
+parser.add_argument('--system_prompt_type', type=str, default="simple", choices=["simple", "cot"], help='System prompt type')
+parser.add_argument("--language", type=str, default="pt-br", choices=["pt-br", "en"], help="Language of the exam")
+parser.add_argument("--max_time", type=int, default=120, help="Max time in seconds to generate an answer")
 parser.add_argument("--seed", type=int, default=42, help="Random seed")
 
 args = parser.parse_args()
@@ -49,6 +51,8 @@ print("Temperature: ", args.temperature)
 print("Answer order: ", args.answer_order)
 print("Question order: ", args.question_order)
 print("System prompt type: ", args.system_prompt_type)
+print("Language: ", args.language)
+print("Max time: ", args.max_time)
 print("Seed: ", args.seed)
 print("\n------------------\n")
 
@@ -57,11 +61,16 @@ print("\n------------------\n")
 device = "cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu"
 
 # Load ENEM exam
-enem = ENEM(args.enem_exam, answer_order=args.answer_order, question_order=args.question_order, seed=args.seed)
+enem = ENEM(args.enem_exam, answer_order=args.answer_order, question_order=args.question_order, seed=args.seed, language=args.language)
 
 # Load model
 if args.model == "llama2":
-    model = LLAMA2(args.model_size, token, device)
+    model = LLAMA2(args.model_size, token, device, max_time=args.max_time, temperature=args.temperature, random_seed=args.seed)
+elif args.model == "mistral":
+    if args.model_size == "7b":
+        model = Mistral(token, device, max_time=args.max_time, temperature=args.temperature, random_seed=args.seed)
+    else:
+        raise Exception("Model size not implemented for Mistral")
 else:
     raise Exception("Model not implemented")
 
@@ -79,14 +88,19 @@ start_time = time.time()
 full_answers = []
 
 for i in range(enem.get_enem_size()):
+    print(f"Question {i}")
+    st = time.time()
     question = enem.get_question(i)
     correct_answer = enem.get_correct_answer(i)
 
-    # TODO: check if I have to skip anulled questions and how I should do it
     if correct_answer == "anulada":
-        continue # Skip anulled questions
+        # Voided question
+        model_response_pattern += "V"
+        correct_response_pattern += "V"
+        model_response_binary_pattern += "0"
+        continue
 
-    model_answer, model_full_answer = model.get_answer_from_question(question, temperature=args.temperature, system_prompt_type=args.system_prompt_type)
+    model_answer, model_full_answer = model.get_answer_from_question(question, system_prompt_type=args.system_prompt_type)
     
     # Remove the prompt from the full answer
     model_full_answer = model_full_answer.split("[/INST]")[-1]
@@ -106,14 +120,16 @@ for i in range(enem.get_enem_size()):
     model_response_pattern += model_answer
     correct_response_pattern += correct_answer
 
+    print(f"Time: {time.time()-st} seconds\n")
+
 end_time = time.time()
 
 # Save results to file
-filename = f"enem-experiments-results/{args.model}-{args.model_size}-{args.temperature}-{args.enem_exam}-{args.answer_order}-{args.question_order}-{args.seed}-{args.system_prompt_type}.parquet"
-df = pd.DataFrame({"MODEL_NAME": [args.model], "MODEL_SIZE": [args.model_size], "TEMPERATURE": [args.temperature], "ANSWER_ORDER": [args.answer_order], "QUESTION_ORDER": [args.question_order], "SEED": [args.seed], "PROMPT_TYPE": [args.system_prompt_type], "CTT_SCORE": [ctt_score], "CO_PROVA": [args.enem_exam], "TX_RESPOSTAS": [model_response_pattern], "TX_GABARITO": [correct_response_pattern], "RESPONSE_PATTERN": [model_response_binary_pattern], "TOTAL_RUN_TIME_SEC": [end_time-start_time], "AVG_RUN_TIME_PER_ITEM_SEC": [(end_time-start_time)/enem.get_enem_size()]})
+filename = f"enem-experiments-results/{args.model}-{args.model_size}-{args.temperature}-{args.enem_exam}-{args.answer_order}-{args.question_order}-{args.seed}-{args.system_prompt_type}-{args.max_time}-{args.language}.parquet"
+df = pd.DataFrame({"MODEL_NAME": [args.model], "MODEL_SIZE": [args.model_size], "TEMPERATURE": [args.temperature], "ANSWER_ORDER": [args.answer_order], "QUESTION_ORDER": [args.question_order], "SEED": [args.seed], "PROMPT_TYPE": [args.system_prompt_type], "CTT_SCORE": [ctt_score], "CO_PROVA": [args.enem_exam], "TX_RESPOSTAS": [model_response_pattern], "TX_GABARITO": [correct_response_pattern], "RESPONSE_PATTERN": [model_response_binary_pattern], "TOTAL_RUN_TIME_SEC": [end_time-start_time], "AVG_RUN_TIME_PER_ITEM_SEC": [(end_time-start_time)/enem.get_enem_size()], "MAX_TIME_SEC": [args.max_time], "LANGUAGE": [args.language]})
 df.to_parquet(filename)
 
 # Saving the full answers to a parquet file (each answer is a row)
-filename = f"enem-experiments-results/{args.model}-{args.model_size}-{args.temperature}-{args.enem_exam}-{args.answer_order}-{args.question_order}-{args.seed}-{args.system_prompt_type}-full-answers.parquet"
+filename = f"enem-experiments-results/{args.model}-{args.model_size}-{args.temperature}-{args.enem_exam}-{args.answer_order}-{args.question_order}-{args.seed}-{args.system_prompt_type}-{args.max_time}-{args.language}-full-answers.parquet"
 df = pd.DataFrame({"FULL_ANSWER": full_answers})
 df.to_parquet(filename)
