6f65d2dd28bfcc2bf2b4ea45b41180439406f39c
gaabrielfranco <gabrielvsfranco@outlook.com>
2023-12-15 15:18:54 -0500

    ENEM results

--- run_experiment_enem_13b.sh
+++ run_experiment_enem_13b.sh
@@ -2,8 +2,8 @@
 
 # Set SCC project
 
-# Submit an array job with 720 tasks
-#$ -t 1-1
+# Submit an array job with 22 tasks
+#$ -t 1-22
 
 # Specify hard time limit for the job.
 #   The job will be aborted if it runs longer than this time.
@@ -30,9 +30,9 @@ for model in "llama2"
 do
     for model_size in "13b"
     do
-        for enem_exam in "ENEM_2022_MT_CO_PROVA_1082"
+        for enem_exam in "ENEM_2022_LC_CO_PROVA_1072" "ENEM_2022_CN_CO_PROVA_1092" "ENEM_2022_CH_CO_PROVA_1062" "ENEM_2017_CH_CO_PROVA_408" "ENEM_2017_CN_CO_PROVA_407" "ENEM_2017_LC_CO_PROVA_409" "ENEM_2017_MT_CO_PROVA_410" "ENEM_2018_CH_CO_PROVA_464" "ENEM_2018_CN_CO_PROVA_463" "ENEM_2018_LC_CO_PROVA_465" "ENEM_2018_MT_CO_PROVA_466"
 		do
-            for system_prompt_type in "cot"
+            for system_prompt_type in "simple" "cot"
             do
                 params[idx]=$model$IFS$model_size$IFS$enem_exam$IFS$system_prompt_type
                 ((idx++))
@@ -51,7 +51,7 @@ module load cuda/11.8
 source /project/mcnet/venv3.10/bin/activate
 cd /projectnb/mcnet/irt-llms
 
-python3 run_enem_exam.py --model ${taskinput[0]} --model_size ${taskinput[1]} --enem_exam ${taskinput[2]} --system_prompt_type ${taskinput[3]} --max_time "600"
+python3 run_enem_exam.py --model ${taskinput[0]} --model_size ${taskinput[1]} --enem_exam ${taskinput[2]} --system_prompt_type ${taskinput[3]}
 
 # index=1
 # for i in "${params[@]}"; do # access each element of array
