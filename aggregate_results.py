b5841b9727df5616d7d24c46dc7271807c5b5003
gaabrielfranco <gabrielvsfranco@outlook.com>
2024-01-09 11:15:30 -0500

    Changes to address questions answers that were not parsed

--- aggregate_results.py
+++ aggregate_results.py
@@ -1,163 +1,72 @@
 import glob
 import re
 from matplotlib import pyplot as plt
+import numpy as np
 import pandas as pd
 import seaborn as sns
+from exam import ENEM
 
-def parse_answer(answer):
-    """
-    Parser is not correct. Example:
+def parse_answer(answer, question, system_prompt_type, language):
+    answer_word = "Answer:" if language == "en" else "Resposta:"
+    regex = r"(?:|[Aa]lternativa|[Oo]pção )" if language == "pt" else r"(?:|[Aa]lternative|[Oo]ption|[Cc]orrect [Aa]nswer is )"
+    ans = answer.split('[/INST]')[-1]
 
-        Aponte as alternativas que fazem sentido: (A), (B), (C), (D).
-
-    Escolha a alternativa CORRETA: (B) 74,51.
-
-    Justifique: O gráfico mostra que a esperança de vida ao nascer em 2013 foi exatamente a média das registradas nos anos de 2012 e de 2014. Então, a esperança de vida ao nascer em 2014 seria a média das registradas nos anos de 2013 e de 2015. A média de 74,23 e 74,51 é 74,32. Então, a esperança de vida ao nascer em 2014 seria 74,32.
-
-    Resposta: (B) 74,51.
-
-    The answer should be B, but the parser returns A.
-
-    def get_formatted_answer(question, answer):
-        gold = ["A.", "B.", "C.", "D.", "E."][question["gold"]]
-        pred = answer
-
-        # regex processing. Useful for zero-shot
-        match_1 = re.findall(r"(?:|[Ll]etra |[Aa]lternativa )([ABCDE])\.", pred)
-        match_2 = re.findall(r"(?:|[Ll]etra |[Aa]lternativa )([ABCDE])", pred)
-        if len(match_1) > 0:
-            pred = match_1[-1] + "."
-        elif len(match_2) > 0:
-            pred = match_2[-1] + "."
+    if system_prompt_type == "cot":
+        match = re.findall(r"{} (\([ABCDE]\))\n".format(answer_word), ans)
+        if len(set(match)) == 1:
+            return match[0].removeprefix("(").removesuffix(")")
         else:
-            print(f"Regex failed at processing {pred=}")
-            print(f"{gold=}, {pred=}")
-
-        return pred, gold
-
-    """
-    pos_inst = answer.split('[/INST]')[-1]
-    ans = pos_inst.strip()
-
-    print("ans")
-    print(ans)
-    a = input()
-
-    pattern = r'Resposta: ([A-E])'
-    match = re.search(pattern, ans)
-    if match:
-        return match.group(1)
-    
-    print("1")
-    
-    pattern = r'Resposta: \(([A-E])\)'
-    match = re.search(pattern, ans)
-    if match:
-        return match.group(1)
-    
-    print("1")
-    
-    pattern = r'Answer: ([A-E])'
-    match = re.search(pattern, ans)
-    if match:
-        return match.group(1)
-    
-    print("1")
-    
-    pattern = r'Answer: \(([A-E])\)'
-    match = re.search(pattern, ans)
-    if match:
-        return match.group(1)
-    
-    print("1")
-
-    pattern = r'answer is ([A-E])'
-    match = re.search(pattern, ans)
-    if match:
-        return match.group(1)
-    
-    print("1")
-    
-    pattern = r'answer is \(([A-E])\)'
-    match = re.search(pattern, ans)
-    if match:
-        return match.group(1)
-    
-    print("1")
-    
-    pattern = r'Answer: ([ABCDE])'
-    match = re.search(pattern, ans)
-    if match:
-        return match.group(1)
-    
-    print("1")
-    
-    pattern = r'A resposta correta é (\w):'
-    match = re.search(pattern, ans)
-    if match:
-        return match.group(1)
-    
-    print("1")
-    
-    pattern = r'[Tt]he answer is ([A-E])'
-    match = re.search(pattern, ans)
-    if match:
-        return match.group(1)
-    
-    print("1")
-    
-    pattern = r'answer is \(([A-E])\)'
-    match = re.search(pattern, ans)
-    if match:
-        return match.group(1)
-    
-    print("1")
-        
-    pattern = r'A resposta correta é (\w) '
-    match = re.search(pattern, ans)
-    if match:
-        return match.group(1)
-    
-    print("1")
-
-    pattern = r'A resposta certa é (\w):'
-    match = re.search(pattern, ans)
-    if match:
-        return match.group(1)
-    
-    print("1")
-    
-    pattern = r'option ([A-E])'
-    match = re.search(pattern, ans)
-    if match:
-        return match.group(1)
-    
-    print("match: ", re.match(r"^(A|B|C|D|E)$", ans))
-    a = input()
-
-    if re.match(r"^(A|B|C|D|E)$", ans):
-        return ans
-    
-    pattern = r'\(([A-E])\)'
-    match = re.search(pattern, ans)
-    print(match)
-    a = input()
-    if match:
-        return match.group(1)
-
-    ans = re.search('[ABCDE]\.',ans).group().rstrip('.') if re.search('[ABCDE]\.',ans) else None
-    print(ans)
-    a = input()
-    
-    if ans is None:
-        pos_inst = answer.split('[/INST]')[-1]
-        ans = pos_inst.strip()
-        ans = re.search('[ABCDE]\)',ans).group().rstrip(')') if re.search('[ABCDE]\)',ans) else None
-        print(ans)
-        a = input()
-
-    return ans
-
+            match = re.findall(r"{} [ABCDE]\)\n".format(answer_word), ans)
+            if len(set(match)) == 1:
+                return match[0]
+            else:
+                match = re.findall(r"{} [ABCDE]".format(answer_word), ans)
+                if len(set(match)) == 1:
+                    return match[0]
+                    
+        if len(ans.split(answer_word)) == 1:
+            match = re.findall(r"{}(\([ABCDE]\))".format(regex), ans)
+            if len(set(match)) == 1:
+                return match[0].removeprefix("(").removesuffix(")")
+            elif len(set(match)) > 1:
+                return None
+            else:
+                match = re.findall(r"{}([ABCDE]\))".format(regex), ans)
+                if len(set(match)) == 1:
+                    return match[0]
+                elif len(set(match)) > 1:
+                    return None
+                else:
+                    match = re.findall(r"{}([ABCDE])".format(regex), ans)
+                    if set(match) == 1:
+                        return match[0]
+                    else:
+                        return None
+        else:
+            ans = ans.split(answer_word)[-1].strip()
+            match = re.findall(r"(\([A-E]\))", ans)
+            if len(set(match)) == 1:
+                return match[0].removeprefix("(").removesuffix(")")
+            elif len(set(match)) > 1:
+                return None
+            else:
+                match = re.findall(r"([A-E]\))", ans)
+                if len(set(match)) == 1:
+                    return match[0].removesuffix(")")
+                elif len(set(match)) > 1:
+                    return None
+                else:
+                    match = re.findall(r"([A-E])", ans)
+                    if len(set(match)) == 1:
+                        return match[0]
+                    else:
+                        return None # Parser error
+    else:
+        match = re.findall(r"(\([A-E]\))", ans)
+        if len(set(match)) == 1:
+            return match[0]
+        else:
+            return None
 
 files = glob.glob("enem-experiments-results/*")
 # removing files with full-answers in the name
@@ -165,32 +74,110 @@ files = [file for file in files if "full-answers" not in file]
 files.sort()
 
 new_df = None
+count = 0
+total = 0
 for file in files:
+    print("Processing file ", file)
+    print()
+    new_file = file.replace("enem-experiments-results", "enem-experiments-results-new-parsing")
+    # if new_file exists
+    # if glob.glob(new_file):
+    #     print("File already exists. Skipping...")
+    #     print("\n\n")
+    #     continue
+
     df = pd.read_parquet(file)
-    # Checking if there is any parsing error
-    if "X" in df.iloc[0, :]["TX_RESPOSTAS"]:
-        print("Parsing error in file ", file)
-        df_full_answers = pd.read_parquet(file.replace(".parquet", "-full-answers.parquet"))
-        if df_full_answers["PARSED_ANSWER"].isnull().sum() > 0:
-            print(df_full_answers[df_full_answers["PARSED_ANSWER"].isnull()].FULL_ANSWER.values[0])
-        else:
-            print(df_full_answers)
-            print(df.iloc[0, :])
-            print()
-            # print(df_full_answers.iloc[32, :].FULL_ANSWER)
-            # print()
-            # print("parsed:", df_full_answers.iloc[32, :].PARSED_ANSWER)
-            # print()
-            #print(parse_answer(df_full_answers.iloc[32, :].FULL_ANSWER))
+    df_full_answers = pd.read_parquet(file.replace(".parquet", "-full-answers.parquet"))
 
-        a = input()
-        print()
+    enem_exam = df.iloc[0, :].ENEM_EXAM
+    enem_exam_type = df.iloc[0, :].ENEM_EXAM_TYPE
+    question_order = df.iloc[0, :].QUESTION_ORDER
+    language = df.iloc[0, :].LANGUAGE
+    seed = df.iloc[0, :].SEED
+    number_options = df.iloc[0, :].NUMBER_OPTIONS
 
-    if new_df is None:
-        new_df = df
-    else:
-        # Concatenate dataframes
-        new_df = pd.concat([new_df, df]).reset_index(drop=True) 
+    if df.iloc[0, :].SYSTEM_PROMPT_TYPE != "cot":
+        continue
+
+    enem = ENEM(enem_exam, exam_type=enem_exam_type, question_order=question_order, seed=seed, language=language, number_options=number_options)
+
+    new_TX_RESPOSTAS = ""
+    new_RESPONSE_PATTERN = ""
+    new_CTT_SCORE = 0
+
+    # Iterate over df_full_answers
+    for idx, row in df_full_answers.iterrows():
+        full_answer = row.FULL_ANSWER
+        parsed_answer = row.PARSED_ANSWER
+        previous_correct_answer = row.CORRECT_ANSWER
+        question = enem.get_question(idx)
+        new_correct_answer = parse_answer(full_answer, question, df.iloc[0, :].SYSTEM_PROMPT_TYPE, language)
+        
+        total += 1
+        if new_correct_answer != previous_correct_answer:
+            # print("\n\n\nQuestion ", idx)
+            # print("Previous correct answer:", previous_correct_answer)
+            # print("New correct answer:", new_correct_answer)
+            # print("\nFull answer:", full_answer)
+
+            if new_correct_answer is None:
+                #new_correct_answer = input("Enter the new correct answer: ")
+                #print(new_correct_answer)
+                count += 1
+
+        # new_TX_RESPOSTAS += new_correct_answer
+        # new_RESPONSE_PATTERN += "1" if new_correct_answer == parsed_answer else "0"
+        # new_CTT_SCORE += 1 if new_correct_answer == parsed_answer else 0
+
+    df["TX_RESPOSTAS"] = new_TX_RESPOSTAS
+    df["RESPONSE_PATTERN"] = new_RESPONSE_PATTERN
+    df["CTT_SCORE"] = new_CTT_SCORE
+
+    # df.to_parquet(new_file)
+
+    # print("Saved to ", new_file)
+    # print("\n\n")
+
+print("Total:", total)
+print("Count:", count)
+print("Percentage:", count/total)
+
+# Old aggregate_results.py
+# files = glob.glob("enem-experiments-results/*")
+# # removing files with full-answers in the name
+# files = [file for file in files if "full-answers" not in file]
+# files.sort()
+
+# new_df = None
+# for file in files:
+#     df = pd.read_parquet(file)
+#     # Checking if there is any parsing error
+#     if "X" in df.iloc[0, :]["TX_RESPOSTAS"]:
+#         print("Parsing error in file ", file)
+#         df_full_answers = pd.read_parquet(file.replace(".parquet", "-full-answers.parquet"))
+#         if df_full_answers["PARSED_ANSWER"].isnull().sum() > 0:
+#             #print(df_full_answers[df_full_answers["PARSED_ANSWER"].isnull()].FULL_ANSWER.values[0])
+#             continue
+#         else:
+#             #print(df_full_answers)
+#             idx = df.iloc[0, :].TX_RESPOSTAS.find("X")
+#             print(df_full_answers.iloc[idx, :].FULL_ANSWER)
+#             print()
+#             # print("parsed:", df_full_answers.iloc[32, :].PARSED_ANSWER)
+#             print()
+#             spt, lang = df.iloc[0, :].SYSTEM_PROMPT_TYPE, df.iloc[0, :].LANGUAGE
+#             print(spt, lang)
+#             print()
+#             print(parse_answer(df_full_answers.iloc[idx, :].FULL_ANSWER, None, spt, lang))
+
+#         a = input()
+#         print()
+
+#     if new_df is None:
+#         new_df = df
+#     else:
+#         # Concatenate dataframes
+#         new_df = pd.concat([new_df, df]).reset_index(drop=True) 
 
 #new_df.to_parquet("enem-experiments-results-processed.parquet")
 
